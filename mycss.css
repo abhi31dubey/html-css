21e079c87ffeb80b723471b848d300ba98f2ba66
abhi31dubey <abhishekdubey31@gmail.com>
2014-06-19 18:21:22 +0530

    Create mycss.css

---
+++ mycss.css
@@ -0,0 +1,81 @@
+@charset "utf-8";
+/* CSS Document */
+body
+{
+	margin:0px;
+}
+#bk
+{
+	background-image:url(grdiantback.jpg);
+	background-repeat:no-repeat;
+	background-size: 100% 350px;
+
+}
+.cf:before,.cf:after
+{
+	content:"";
+	display:table;
+}
+.cf:after
+{
+	clear:both;
+}
+.cf
+{
+	zoom:1;
+}
+#nav
+{
+	background:#916A31;
+	height:2.3em;
+}
+#nav ul li
+{
+	list-style:none;
+	margin:0px;
+	padding:0px;
+	float:left;
+}
+#nav ul
+{
+	margin:0px;
+	padding:0px;
+	background:#D5973C;
+	line-height:2em;
+	width:100%;
+	float:left;
+}
+#nav ul li a
+{
+	display:block;
+	height:2em;
+	padding:0 1em;
+	color: #FFF;
+	text-decoration:none;
+}
+#nav ul li a:hover
+{
+	background-color:#916A31;
+	top:-.4em;
+	position:relative;
+	border-radius:.4em .4em 0 0;
+
+}
+#nav ul li:hover
+{
+	background-color:#916A31;
+}
+#nav ul li.scur,#nav ul li.scur:hover
+{
+	background-color:#AD9B7f;
+	color:#eee;
+}
+.current,#nav ul li a:hover.current
+{
+	background-color:#AD9B7f;
+	color:#eee;
+	top:-.4em;
+	position:relative;
+	border-radius:.4em .4em 0 0;
+	
+}
